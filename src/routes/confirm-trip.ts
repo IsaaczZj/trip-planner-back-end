74113638a66921c52f5735a2bf66d022b6cdb477
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-07-28 17:33:22 -0300

    feat:creating dayJs function

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import {z} from "zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -8,11 +10,41 @@ export async function confirmTrip(app: FastifyInstance) {
     {
       schema: {
         params: z.object({
-          tripId:z.uuid()
+          tripId: z.uuid(),
         }),
       },
     },
-    async (request) => {
+    async (request, reply) => {
+      const { tripId } = request.params;
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+      });
+
+      if (!trip) {
+        throw new Error("Viajem não encontrada");
+      }
+      if (trip.isConfirmed) {
+        return reply.redirect(`http://localhost:5173/trips/${tripId}}`);
+      }
+
+      await prisma.trip.update({
+        where: {
+          id: tripId,
+        },
+        data: {
+          isConfirmed: true,
+        },
+      });
+
+      // const formattedStartDate = dayjs(starts_at).format("LL");
+      // const formattedEndDate = dayjs(ends_at).format("LL");
+
+      const confirmationLink = `http://localhost:3333/trips/${newTrip.id}/confirm`;
+
+      const mail = await getMaillient();
+
       return { tripId: request.params.tripId };
     }
   );
